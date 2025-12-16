ef6006bd02179de907ac3bf2467781473748fce1
kethankoushik04 <kethankoushik04@example.com>
2025-12-16 16:29:06 +0530

    put credentials

--- server.js
+++ server.js
@@ -3,39 +3,25 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
-// dotenv.config();
 connectDB();
-
-const allowedOrigins = [
-  "http://localhost:5173", // Vite
-  "https://event-management-web-ivory.vercel.app/"
-];
 
 const app = express();
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: "https://event-management-web-ivory.vercel.app",
   credentials: true,
 }));
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
   res.send("Kethan Gaikwad");
-})
+});
 
-
-app.use('/api/profiles', require('./routes/profileRoutes'));
-app.use('/api/events', require('./routes/eventRoutes'));
+app.use("/api/profiles", require("./routes/profileRoutes"));
+app.use("/api/events", require("./routes/eventRoutes"));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
 });
