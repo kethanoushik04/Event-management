c3d01f4c98ba57798a1f7df5197c8bae20ec5fda
kethankoushik04 <kethankoushik04@example.com>
2025-12-16 15:01:55 +0530

    Fix MongoDB connection using env variables

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/event_mgmt");
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected Successfully");
   } catch (error) {
     console.error("MongoDB Connection Failed", error);
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
+require("./controllers/env.js");
 const express = require("express");
-// const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
 
